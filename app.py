faee5db8f631b6c50a49159bb84ff456626c57bb
Kameon <147346348+Kameonx@users.noreply.github.com>
2025-03-04 23:06:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         url = request.form['url']
+        format = request.form['format']
         try:
             # Create a temporary directory to store the video file
             with tempfile.TemporaryDirectory() as temp_dir:
                 ydl_opts = {
-                    'format': 'best[ext=mp4]',  # Select the best mp4 format
                     'quiet': True,
                     'no_warnings': True,
                     'noprogress': True,
@@ -22,6 +22,17 @@ def index():
                     'outtmpl': os.path.join(temp_dir, '%(title)s.%(ext)s'),  # Use a template for the output file
                     'useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'  # Add a User-Agent
                 }
+                
+                if format == 'mp4':
+                    ydl_opts['format'] = 'best[ext=mp4]'  # Select the best mp4 format
+                elif format == 'mp3':
+                    ydl_opts['format'] = 'bestaudio/best'
+                    ydl_opts['postprocessors'] = [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                    }]
+                
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     info_dict = ydl.extract_info(url, download=False)
                     
@@ -33,6 +44,9 @@ def index():
                     
                     # Find the downloaded file in the temporary directory
                     filename = ydl.prepare_filename(info_dict)
+                    if format == 'mp3':
+                        filename = os.path.splitext(filename)[0] + '.mp3'
+                    
                     with open(filename, 'rb') as f:
                         video_data = f.read()
                     
@@ -42,7 +56,10 @@ def index():
                     # Reset buffer position to the start
                     buffer.seek(0)
                     
-                    return send_file(buffer, as_attachment=True, download_name=f"{title}.mp4", mimetype='video/mp4')
+                    if format == 'mp4':
+                        return send_file(buffer, as_attachment=True, download_name=f"{title}.mp4", mimetype='video/mp4')
+                    elif format == 'mp3':
+                        return send_file(buffer, as_attachment=True, download_name=f"{title}.mp3", mimetype='audio/mpeg')
 
         except Exception as e:
             return render_template('error.html', error=str(e))
